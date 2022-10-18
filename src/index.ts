9994409571e4f1b8b5f435578e00d614b05ef42c
Michał M. Sapka <michal@sapka.me>
2022-10-18 12:01:36 +0200

    feat: add autoupdate script

--- src/index.ts
+++ src/index.ts
@@ -52,3 +52,8 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+require('update-electron-app')({
+  repo: 'michalsapka/emodoro',
+  updateInterval: '1 hour',
+  logger: require('electron-log')
+}
